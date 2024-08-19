2c01d12d5cc69cdfca24ea2213a5dae2e399d0b0
VitorHugoFMartins <vitorferreira010vhfm@gmail.com>
2024-08-19 20:33:07 -0300

    Criando um Sistema e Abstraindo um Celular com POO em C#
    Abstraindo um telefone celular para código usando Progamação Orientada a Objetos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero: "1929201823901", modelo: "Nokia XR21", imei: "1203940205910294", memoria: 128);
+Smartphone iphone = new Iphone(numero: "9348502784018203", modelo: "Iphone 15", imei: "190283720273827618", memoria: 256);
+
+Console.WriteLine("Smartphone Nokia XR21");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Youtube");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone 15");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
